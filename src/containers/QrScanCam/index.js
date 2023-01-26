9f182196d39068869cce1e63c3b5740f1d6b0dee
br1giacometti <78874204+br1giacometti@users.noreply.github.com>
2023-01-26 18:08:50 -0300

    Qr Scanner CAm Listo

--- src/containers/QrScanCam/index.js
+++ src/containers/QrScanCam/index.js
@@ -1,43 +1,64 @@
 import React, { useState } from 'react';
-import { collection, doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
-import { db } from '../../services';
-import QRScanner from 'react-qr-scanner';
+
+
+import QRScanner from 'react-qr-scanner'
+
+
+import { getDoc, updateDoc, doc, addDoc, collection } from "firebase/firestore"
+import { db } from '../../services/firebase';
 
 
 
-const qrsCollection = collection(db, "qrs");
-
-const QrByScanCam = () => {
-    // const dispatch = useDispatch();
-     //const { id } = router.query;
-    const router = useRouter();
 
 
+
+function QrByScanCam() {
     const [qrData, setQrData] = useState('');
-    const [render, setRender] = useState('');
+    //const history = useHistory();
 
+
+
+    //Muestra o no QR
+    const [ check, setCheck ] = useState('')
+    const [ checkHandle, setCheckHandle ] = useState(0)
+
+    //Set QR Date
+    const [ fechaLimite, setFechaLimite ] = useState('')
+    const [ tipoUso, setTipoUso ] = useState('')
+    const [ cantidadGenerada, setCantidadGenerada ] = useState(0)
+    const [ cantidadVecesUsado, setCantidadVecesUsado ] = useState(0)
+
+    const [ evento, setEvento ] = useState('')
+    const [ creador, setCreador ] = useState('')
+    const [ destinatario, setDestinatario ] = useState('')
+    const [ id, setIdQr ] = useState('')
+   
     
-
-   //   useEffect(() => {
-     //   dispatch(suscribeQrID());
-     // }, []);
+  
 
 
-async function handleScan2(data) {
+   
+  
+
+
+
+
+    function handleError(err) {
+        console.log(err);
+    }
+
+    async function handleScan(data) {
         if (data) {
             setQrData(data);
             try {
-                const doc = await qrsCollection.doc(data).get()
+                //const doc = await firestore.get({ collection: 'collectionName', doc: data });
+                //
                 if (doc.exists) {
                     const data = doc.data();
-                    product.data().evento === "Brutus park" ? setRender("verde") : setRender("rojo")
                     //validate the data
-                  // if(data.isValid){
-                    if(data.exists){
-                        //history.push('/successPage');
-                        //router.replace(`/qrs/`);
-                        setQrData("hola")
+                    if(data.isValid){
+                       // history.push('/successPage');
                     }
                 }
             } catch (error) {
@@ -46,43 +67,93 @@ async function handleScan2(data) {
         }
     }
 
+    /*async function handleScan2(id) {
+        if (id) {
+            const productSnapshot = await getDoc( doc(db, "qrs", id) )
+            if(productSnapshot.exists) {
+                console.log(productSnapshot.data().evento) 
+            }else{
+                console.log('El producto no existe')
+            }
+        } else {
+            console.log("No data received");
+        }
+    }*/
 
-     function handleScan(data) {
 
-            setQrData(data);
+    const getProductById = async (id) => {
+        if (id === null) {
+            console.log("El id no puede ser nulo");
+            return;
+        }
+        
+        const product = await getDoc( doc(db, "qrs", id.text) )
+        if(product.exists() && checkHandle === 0) {
+            console.log(product.data())
+            // seteo los valores a los useStates
+            setIdQr(id.text);
+            setFechaLimite(product.data().fechaLimite);
+            setCantidadGenerada(product.data().cantidadGenerada);
+            setCantidadVecesUsado(product.data().cantidadVecesUsado);
+            setEvento(product.data().evento);
+            setCreador(product.data().creador);
+            setDestinatario(product.data().destinatario);
+            // obtener valores de la base de datos
+            const fechaLimite = product.data().fechaLimite;
+            const cantidadGenerada = product.data().cantidadGenerada;
+            const cantidadVecesUsado = product.data().cantidadVecesUsado;
+            // convertir fecha limite a fecha
+            const fechaLimiteFormat = new Date(fechaLimite);
+            // obtener fecha actual
+            const fechaActual = new Date();
+            // comparar fechas
+            if (fechaActual >= fechaLimiteFormat ||  cantidadVecesUsado >= cantidadGenerada) {
+                console.log("No podes pasar");
+                setCheck("No")
+            } else {
+                // hacer algo si se cumple la condicion
 
+
+                console.log("podes pasar");
+                const product = doc(db, "qrs", id.text)
+        const data = {cantidadVecesUsado: +cantidadVecesUsado + 1}
+        setCheckHandle(1) 
+        await updateDoc(product, data) + console.log("updateando qr")
+setCheck("Si")
+      
+
+            }
+        }else{
+            console.log('El producto no existe')
+        }
     }
 
 
-  return (
- 
 
-<div>
-{render === "" ?
+    const updateQR = async (id) => {
 
-<QRScanner onScan={handleScan}/>
-
-
+        const product = doc(db, "qrs", id)
+        const data = {evento: evento,fechaLimite: fechaLimite, creador: creador, destinatario: destinatario, tipoUso: tipoUso, cantidadGenerada: cantidadGenerada, cantidadVecesUsado: cantidadVecesUsado + 1}
+        await updateDoc(product, data)
+        console.log("qr actualziado");
+    }
     
-    
-    : "" }
 
 
-{render== "verde" ?
+    return (
+        <div>
 
-"PODES PASAR"
-    
-    : render== "rojo" ? "NO PODES PASAR" : "" }
+        {check === "" ?
+            <QRScanner onScan={getProductById} onError={handleError} /> : ""
+        }
 
-  
+{check === "Si" ? 
+    <div> Podes pasar : <button onClick={() => setCheck("")}>Volver</button>  </div> : check === "No" ? <div> No Podes pasar : <button onClick={() => setCheck("")}>Volver</button> </div>: ""
+}
 
-{qrData}
-</div>
-
-);
+            
+        </div>
+    );
 }
 
 export default QrByScanCam;
-
-
-
