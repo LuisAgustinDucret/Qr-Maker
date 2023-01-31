1fdf6963b1d7e1f1148ff51bc74f8447ad0c7b54
br1giacometti <78874204+br1giacometti@users.noreply.github.com>
2023-01-30 21:08:41 -0300

    PDF Creator Andanndo

--- src/containers/QrId/index.js
+++ src/containers/QrId/index.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
-import QrGenerate from "../../components/QrGenerate";
+import QRCode from 'qrcode';
 import { suscribeQrID } from "../../store/actions/";
 import { getKeys, mapPropertiesToColumns } from "../../utils";
 import { QrsDataTable } from "../../entities";
@@ -14,12 +14,17 @@ import {
   DataOcultaContainer,
   ButtonContainer,
   ul,
+  styles,
 } from "./styles";
+import { PDFDownloadLink, View } from '@react-pdf/renderer';
+import { Document, Page, Image, Text, StyleSheet } from '@react-pdf/renderer';
+import QrGenerate from "../../components/QrGenerate";
 
 const QrId = () => {
   const dispatch = useDispatch();
   const router = useRouter();
   const { id } = router.query;
+  const [base64, setBase64] = useState(null);
 
   const { qrID } = useSelector(({ qr }) => {
     return {
@@ -31,16 +36,19 @@ const QrId = () => {
     dispatch(suscribeQrID(id));
   }, [id]);
 
-  const renderColumn = () => {
-    const columns = getKeys(new QrsDataTable()).map((key) => {
-      return mapPropertiesToColumns(key, 200);
+  const handleQRCode = () => {
+    QRCode.toDataURL(id, (err, url) => {
+      setBase64(url);
     });
-    return columns;
   };
+
+  useEffect(() => {
+    handleQRCode();
+  }, [id]);
 
   return (
     <>
-      {console.log(qrID)}
+        
 
       <Container>
         {qrID.map((qrs) => (
@@ -49,19 +57,19 @@ const QrId = () => {
               <b> {qrs.evento}</b>
             </CardTop>
             <Card>
-              {QrGenerate(id)}
+            {QrGenerate(id)}
               <Line></Line>
               <DataContainer key={`${qrs.id}-DataContainer`}>
                 <div>
                   <ul>
                     <li>
-                      <b>Evento:</b> {qrs.evento}{" "}
+                      <b>Evento:</b> {qrs.evento}
                     </li>
                     <li>
                       <b>Tipo de QR:</b> {qrs.tipoUso}
                     </li>
                     <li>
-                      <b>Fecha Limite:</b> {qrs.fechaLimite}{" "}
+                      <b>Fecha Limite:</b> {qrs.fechaLimite}
                     </li>
                     <li>
                       <b>Limite de Usos:</b> {qrs.cantidadGenerada}
@@ -70,6 +78,7 @@ const QrId = () => {
                 </div>
               </DataContainer>
               <Line></Line>
+
               <DataOcultaContainer key={`${qrs.id}-DataOcultaContainer`}>
                 <ul>
                   <p>
@@ -86,6 +95,41 @@ const QrId = () => {
                   </li>
                 </ul>
               </DataOcultaContainer>
+
+              <Line></Line>
+
+              <div>
+  {base64 && (
+    <PDFDownloadLink
+      document={
+<Document>
+  <Page style={styles.page}>
+  {qrID.map((qrs) => (
+      <div key={`${qrs.id}-div`}>
+        <View style={styles.view}><Image src={base64}  style={styles.QRImage}/></View>
+
+                <View style={styles.view}><Text>Evento: {qrs.evento}</Text></View>
+
+                <View style={styles.view}><Text>Tipo de QR: {qrs.tipoUso}</Text></View>
+
+                <View style={styles.view}><Text>Fecha Limite: {qrs.fechaLimite}</Text></View>
+
+                <View style={styles.view}><Text>Limite de Usos: {qrs.cantidadGenerada}</Text></View>
+
+            </div>
+    ))}
+
+  </Page>
+</Document>
+      }
+      fileName="qrCode.pdf"
+    >
+      {({ blob, url, loading, error }) => (loading ? 'Cargando...' : 'Descargar PDF')}
+    </PDFDownloadLink>
+  )}
+</div>
+
+
             </Card>
           </div>
         ))}
--- src/containers/QrId/styles.js
+++ src/containers/QrId/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { StyleSheet } from '@react-pdf/renderer';
 
 export const Container = styled.div`
 content: "";
@@ -73,3 +74,30 @@ export const ButtonContainer = styled.div`
 
 
 `;
+
+
+export const styles = StyleSheet.create({
+
+    page: {
+        backgroundColor: 'white',
+        display: 'block',
+    },
+
+    view: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+    },
+
+    QRImage: {
+        width: '100%',
+        height: '100%',
+    },
+
+    Text: {
+        color: 'black',
+        size: '11em'
+    }
+    
+    })
